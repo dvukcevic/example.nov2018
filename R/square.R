44f39518469d71c236923d9571b92bf7433fd6ed
Damjan Vukcevic <damjan.vukcevic@unimelb.edu.au>
2018-11-13 16:03:21 +1100

    Initial commit

---
+++ R/square.R
@@ -0,0 +1,33 @@
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+square <- function(x) {
+  x^2
+}
+
+#' Cube of a number
+#'
+#' This calculates the cube of a number.
+#'
+#' @param x Numeric vector
+#'
+#' @return Numeric vector
+#' @export
+#'
+#' @examples
+#' cube(4)
+cube <- function(x) {
+  x * x * x
+}
+
+
+#' @export
+is.odd <- function(x) {
+  gtools::odd(x)
+}
+
